501e272257aa536fcfad596e37c41f3180d78989
Richard Quirante <richardquirante98@gmail.com>
2025-03-07 17:04:43 +0800

    s

--- State/OrderState.cs
+++ State/OrderState.cs
@@ -1,5 +1,6 @@
 ﻿using EBISX_POS.Models;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace EBISX_POS.State
 {
@@ -10,7 +11,27 @@ namespace EBISX_POS.State
         public static OrderItemState CurrentOrderItem { get; set; } = new OrderItemState();
         public static decimal _totalAmount;
 
+        public static void UpdateDrinksOrder(int drinkId, string name, decimal price, string size)
+        {
+            var drink = CurrentOrderItem.SubOrders.FirstOrDefault(c => c.DrinkId == drinkId);
+            if (drink != null)
+            {
+                drink.Name = name;
+                drink.ItemPrice = price;
+                drink.Size = size;
+            }
+            else
+            {
 
+                CurrentOrderItem.SubOrders.Add(new SubOrderItem
+                {
+                    DrinkId = drinkId,
+                    Name = name,
+                    ItemPrice = price,
+                    Size = size
+                });
+            }
+        }
 
     }
 }
